0ae16450004e755c2a7830f7854d0dd5c8efccf4
Karthees <kartheeswaran2201@gmail.com>
2026-03-18 19:44:50 +0530

    updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyCFZSNjMj8j82UiPN2UfmYX0BxgHpnDeUo",
-  authDomain: "portfoilo-2feb4.firebaseapp.com",
-  projectId: "portfoilo-2feb4",
-  storageBucket: "portfoilo-2feb4.firebasestorage.app",
-  messagingSenderId: "910092048979",
-  appId: "1:910092048979:web:96a3f5d3373b44dce5c4a6",
-  measurementId: "G-WTQNW2B0JP"
-};
+
 
 const app = initializeApp(firebaseConfig);
 
