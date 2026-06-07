f3696f49284592acd23e097e6e92af388045faad
Kartheeswaran-git <kartheeswaran2201@gmail.com>
2026-06-07 23:03:56 +0530

    config

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 //config
 const firebaseConfig = {
-  apiKey: "AIzaSyCFZSNjMj8j82UiPN2UfmYX0BxgHpnDeUo",
-  authDomain: "portfoilo-2feb4.firebaseapp.com",
-  projectId: "portfoilo-2feb4",
-  storageBucket: "portfoilo-2feb4.firebasestorage.app",
-  messagingSenderId: "910092048979",
-  appId: "1:910092048979:web:96a3f5d3373b44dce5c4a6",
-  measurementId: "G-WTQNW2B0JP",
+  
 };
 
 const app = initializeApp(firebaseConfig);
